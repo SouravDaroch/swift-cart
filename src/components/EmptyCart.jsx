0332482cf657b1fb9d547951f5dd54eec2ee1057
SouravDaroch <souravdaroch984@gmail.com>
2026-02-04 02:18:25 +0530

    fix: empty-cart image

--- src/components/EmptyCart.jsx
+++ src/components/EmptyCart.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router'
 const EmptyCart = () => {
   return (
    <div className=' col-span-12 h-87 p-30 flex  flex-col justify-center'>
-              <img src="\images\empty-cart.png" alt="" width={"100px"} className='mx-auto'/>
+              <img src="/empty-cart.png" alt="" width={"100px"} className='mx-auto'/>
               <center className='text-gray-700 font-semibold pt-4'>You haven't added anything yet. </center>
               <center className='text-gray-600 '>Let's add some items</center>
              <Link to={"/"} className='mx-auto'>
